56f39d90fe47fb06b1f7d4f4dfe545fb8c778f4f
Wesley Chen <wesleychen44@gmail.com>
2022-04-25 18:19:30 -0400

    Aiming implemented
    Left/right aim not implemented. Also cursor while aiming isn't too good but it could work.

--- Airship Final/Assets/Scripts/Aiming.cs
+++ Airship Final/Assets/Scripts/Aiming.cs
@@ -4,10 +4,78 @@ using UnityEngine;
 
 public class Aiming : MonoBehaviour
 {
-    public Texture2D crosshair; 
+    public Texture2D crosshair;
+    public float maxHorizontalDegrees = 60f; // 90 = 180 degree view
+    public float maxVerticalDegrees = 45f;
+    public float aimRate = 5f;
 
-    void Start() {
-        Vector2 cursorOffset = new Vector2(crosshair.width/2, crosshair.height/2);
+    public bool released; // don't edit in unity pls
+
+    private float currHorz;
+    private float currVert;
+
+    // Camera object; ship; true if looking left else false
+    public IEnumerator Aim(GameObject cam, GameObject ship, bool isLeft) {
+        released = false;
+        currHorz = 0f;
+        currVert = 0f;
+
+
+        // Defines crosshair. Can we lock this to the center of the screen? Cursor.lockState doesn't work idk
+        Vector2 cursorOffset = new Vector2(crosshair.width / 2, crosshair.height / 2);
         Cursor.SetCursor(crosshair, cursorOffset, CursorMode.ForceSoftware);
+
+
+        while (!released) {
+            float mouseX = Input.GetAxis("Mouse X");
+            float mouseY = Input.GetAxis("Mouse Y");
+
+            // Sets camera to current position of ship every time.
+            cam.transform.position = ship.transform.position;
+            cam.transform.rotation = ship.transform.rotation;
+
+            if (isLeft) { 
+                cam.transform.Rotate(0, -90f, 0); // left
+            }
+            else
+            {
+                cam.transform.Rotate(0, 90f, 0); // other left
+            }
+            cam.transform.position += cam.transform.forward * .5f;
+
+            // controls left and right movement of camera while aiming within the limits defined by maxHorz/Vert
+            if (mouseX != 0)
+            {
+                currHorz += mouseX * aimRate;
+                if (currHorz > maxHorizontalDegrees)
+                {
+                    currHorz = maxHorizontalDegrees - 0.1f;
+                }
+                if (currHorz < -maxHorizontalDegrees)
+                {
+                    currHorz = -maxHorizontalDegrees + 0.1f;
+                }
+            }
+            if (mouseY != 0)
+            {
+                currVert += -mouseY * aimRate;
+                if (currVert > maxVerticalDegrees)
+                {
+                    currVert = maxVerticalDegrees - 0.1f;
+                }
+                if (currVert < -maxVerticalDegrees)
+                {
+                    currVert = -maxVerticalDegrees + 0.1f;
+                }
+            }
+
+            // The actual rotation. No touch.
+            cam.transform.Rotate(currVert, currHorz, 0);
+
+            yield return null;
+        }
+
+        Cursor.SetCursor(null, Vector2.zero, CursorMode.ForceSoftware);
+        yield break;
     }
 }
--- Airship Final/Assets/Scripts/CameraControl.cs
+++ Airship Final/Assets/Scripts/CameraControl.cs
@@ -12,21 +12,46 @@ public class CameraControl : MonoBehaviour
     public float maxDistance = 10f;
 
     private float currentDistance;
+    private Vector3 direction;
 
     // Start is called before the first frame update
     void Start()
     {
         transform.position = playerShip.transform.position - new Vector3(0f, 0f, maxDistance);
         currentDistance = maxDistance;
+        direction = transform.position - playerShip.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // On LShift, calls aim coroutine, and suppresses self until LShift release
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            cam.GetComponent<Aiming>().StartCoroutine(cam.GetComponent<Aiming>().Aim(cam, playerShip, IsLeft())); 
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            cam.GetComponent<Aiming>().released = true;
+
+            Reset(currentDistance, direction);
+        }
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            return;
+        }
+
+
+
         float mouseX = Input.GetAxis("Mouse X");
         transform.RotateAround(playerShip.transform.position, transform.up, rotateRate * mouseX);
 
-        if (Input.GetKey(KeyCode.LeftControl)) //currently broken, idk why
+        /* if (mouseX != 0) {
+         * direction = transform.position - playerShip.transform.position;
+         * }  Possible fix for camera automatically recentering when moving too fast, but stutters so idk */
+        direction = transform.position - playerShip.transform.position;
+
+        if (Input.GetKey(KeyCode.LeftControl)) // Allows player to zoom in and out when LCtrl held down
         {
             float mouseY = Input.GetAxis("Mouse Y");
             if (mouseY != 0)
@@ -45,23 +70,24 @@ public class CameraControl : MonoBehaviour
             }
         }
 
-        Vector3 direction = transform.position - playerShip.transform.position;
+        Reset(currentDistance, direction);
+    }
 
+    // Called whenever the camera needs to get moved back to the position
+    private void Reset(float currentDistance, Vector3 direction)
+    {
+        // Maintains the same distance from camera and ship      
         transform.position = direction.normalized * currentDistance + playerShip.transform.position;
-        
-/*
-        if (currentDistance > maxDistance)
-        {
-            
-        }
-        if (distance < minDistance)
-        {
-            transform.position = direction.normalized * minDistance + playerShip.transform.position;
-        }*/
 
-
-
+        // Camera focuses on ship, remains same height
         transform.position = new Vector3(transform.position.x, playerShip.transform.position.y, transform.position.z);
         transform.LookAt(playerShip.transform);
+    }
+
+
+    // This problem is annoying. Need to figure out whether camera is on the left or the right of the ship
+    private bool IsLeft()
+    {
+        return true;
     }
 }
